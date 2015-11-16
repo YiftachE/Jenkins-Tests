8ea9c65bcdac810e5d9ff3b82415b70f73f326e4
יפתח אדלשטיין < >
2015-11-16 21:19:20 +0200

    Adding unit tests to fail

---
+++ Test/UnitTests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GetMeData;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            HttpData data = new HttpData();
+            Assert.AreEqual(data.GetStringJsonData(), "");
+        }
+    }
+}
